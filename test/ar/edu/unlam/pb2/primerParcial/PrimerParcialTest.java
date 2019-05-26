eb41c364872dacb35a5d7f122941944822fddb3b
@cepedaian <cepedaian4@gmail.com>
2019-05-25 21:47:51 -0300

    agregue test

--- test/ar/edu/unlam/pb2/primerParcial/PrimerParcialTest.java
+++ test/ar/edu/unlam/pb2/primerParcial/PrimerParcialTest.java
@@ -25,16 +25,33 @@ public class PrimerParcialTest {
 	@Test
 
 	public void testQueVerifiqueArrancarUnCamion() {
-		
+
 		Boolean valorEsperado = true;
 		Boolean valorObtenido;
-	
-		Direccion miDireccion = new Direccion("",0,"");
-		Fabricante miFabricante = new Fabricante("",miDireccion);
-		Camion miCamion = new Camion("","",0.0,miFabricante,0,0,0,true);
-	
+
+		Direccion miDireccion = new Direccion("", 0, "");
+		Fabricante miFabricante = new Fabricante("", miDireccion);
+		Camion miCamion = new Camion("", "", 0.0, miFabricante, 0, 0, 0, true);
+
 		valorObtenido = miCamion.arrancar();
+
+		Assert.assertEquals(valorEsperado, valorObtenido);
+	}
+
+	@Test
+
+	public void testQueDescargueTodasLasCargas() {
+		Integer valorEsperado = 0;
+		Integer valorObtenido;
+		
+		Direccion miDireccion = new Direccion("", 0, "");
+		Fabricante miFabricante = new Fabricante("", miDireccion);
+		Camion miCamion = new Camion("", "", 0.0, miFabricante, 0,0,200, true);
 	
+		miCamion.descargar();
+		
+		valorObtenido = miCamion.getPesoDeCarga();
+		
 		Assert.assertEquals(valorEsperado, valorObtenido);
 	}
 
